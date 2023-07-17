276f2b3c4fa97a281ee428d70a39d736262a3e4e
ivandjoh <ivandi.djoh@gmail.com>
2023-07-17 20:49:05 +0800

    add search component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['flowbite.com'],
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+}
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import Image from 'next/image'
+"use client"
 
-export default function Home() {
+import React, { JSX } from "react";
+import {SearchComponent} from "@/components/SearchComponent";
+
+const HomePage: React.FC = (): JSX.Element => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h3>Hello World</h3>
+      <div className="z-10 w-full max-w-2xl items-center justify-between font-mono text-sm lg:flex">
+        <SearchComponent />
       </div>
     </main>
   )
 }
+
+export default HomePage
